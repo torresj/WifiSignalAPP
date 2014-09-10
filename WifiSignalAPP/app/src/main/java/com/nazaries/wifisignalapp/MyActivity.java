3ab4e3cf684febaebb108d2aaf0608b312a76a57
Jaime Torres Benavente <jaime.torres@nazaries.net>
2014-09-10 10:56:05 +0200

    Proyecto de Andorid Studio añadido

---
+++ WifiSignalAPP/app/src/main/java/com/nazaries/wifisignalapp/MyActivity.java
@@ -0,0 +1,68 @@
+package com.nazaries.wifisignalapp;
+
+import java.util.List;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MyActivity extends Activity {
+    TextView mainText;
+    WifiManager mainWifi;
+    WifiReceiver receiverWifi;
+    List<ScanResult> wifiList;
+    StringBuilder sb = new StringBuilder();
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        mainText = (TextView) findViewById(R.id.mainText);
+        mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        receiverWifi = new WifiReceiver();
+        registerReceiver(receiverWifi, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        mainWifi.startScan();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, 0, 0, "Refresh");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        mainWifi.startScan();
+        mainText.setText("Starting Scan");
+        return super.onMenuItemSelected(featureId, item);
+    }
+
+    protected void onPause() {
+        unregisterReceiver(receiverWifi);
+        super.onPause();
+    }
+
+    protected void onResume() {
+        registerReceiver(receiverWifi, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        super.onResume();
+    }
+
+    class WifiReceiver extends BroadcastReceiver {
+        public void onReceive(Context c, Intent intent) {
+            sb = new StringBuilder();
+            ScanResult result=null;
+            wifiList = mainWifi.getScanResults();
+            for(int i = 0; i < wifiList.size(); i++){
+                result=wifiList.get(i);
+                sb.append("Red:"+result.SSID+" Señal:"+result.level);
+                sb.append("\n");
+            }
+            mainText.setText(sb);
+            mainWifi.startScan();
+        }
+    }
+}
